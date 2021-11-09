dcc213dcf0ee301dc0875c1b91f076468e1cf722
Hubert MUGABO <mugberto@gmail.com>
2021-11-09 18:46:18 +0200

    Add route for random message endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1, default: { format: 'json' } do
+    get 'random-message', to: 'messages#show_random_message'
+  end
 end
